5f6eb9af2cc50977f9372a38858663165c5b85a0
Bluesik <blues1905@gmail.com>
2018-09-14 16:17:36 +0200

    Add self / voice properties to a Cast object

--- src/Data/Cast.php
+++ src/Data/Cast.php
@@ -8,6 +8,8 @@ class Cast extends TVMazeResource
 {
     public $person;
     public $character;
+    public $self;
+    public $voice;
 
     public function __construct ($data = []){
         if(! $data){
@@ -16,5 +18,7 @@ class Cast extends TVMazeResource
 
         $this->person    = array_key_exists('person', $data)    ? new Person($data['person'])       : null;
         $this->character = array_key_exists('character', $data) ? new Character($data['character']) : null;
+        $this->self      = array_key_exists('self', $data)      ? $data['self']                     : null;
+        $this->voice     = array_key_exists('voice', $data)     ? $data['voice']                    : null;
     }
 }
